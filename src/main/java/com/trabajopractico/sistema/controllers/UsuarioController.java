a8eee30166a6a72638dedde1654740ea8c2b0704
Facundo Lopez <56969887+f4cuL@users.noreply.github.com>
2022-05-09 20:44:16 -0300

    Allow cors

--- src/main/java/com/trabajopractico/sistema/controllers/UsuarioController.java
+++ src/main/java/com/trabajopractico/sistema/controllers/UsuarioController.java
@@ -7,6 +7,7 @@ import com.trabajopractico.sistema.entities.Usuario;
 import com.trabajopractico.sistema.service.UsuarioService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@CrossOrigin(origins = "*")
 public class UsuarioController {
     @Autowired
     UsuarioService usuarioService;
