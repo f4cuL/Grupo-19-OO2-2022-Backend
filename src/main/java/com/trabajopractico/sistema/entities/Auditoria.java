383644455289880e30efed615b70f22bbaae4aff
Facundo Lopez <56969887+f4cuL@users.noreply.github.com>
2022-05-09 01:04:16 -0300

    Auditoria pasa a ser entidad

--- src/main/java/com/trabajopractico/sistema/entities/Auditoria.java
+++ src/main/java/com/trabajopractico/sistema/entities/Auditoria.java
@@ -1,15 +1,11 @@
 package com.trabajopractico.sistema.entities;
 
 import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
 
+@Entity
 @DiscriminatorValue("auditoria")
 public class Auditoria extends Usuario{
     
 }
-/*Cada usuario puede tener un único perfil asignado, y los mismos deben ser definidos previos a la vinculación con el
-usuario. Se solicitan al menos dos tipos de perfiles: administrador y auditoría. Cada vista o sección del sistema deberá
-verificar el perfil del usuario logeado para habilitar / deshabilitar acciones según corresponda.
-El sistema deberá permitir la alta, baja y modificación tanto de los usuarios como de los perfiles para aquellos usuarios
-con perfil administrador. Y en el caso de los usuarios con perfil de auditoria los mismos podrán consultar tanto el listado
-de los usuarios como el de administradores y descargar la información en un documento con formato PDF.
-*/
+
