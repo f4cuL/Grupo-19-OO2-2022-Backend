0ffcb510941ce8fb804de6001a585a25df5002c2
Facundo Lopez <56969887+f4cuL@users.noreply.github.com>
2022-05-11 18:23:41 -0300

    hotfix

--- src/main/java/com/trabajopractico/sistema/service/WebSecurityConfiguration.java
+++ src/main/java/com/trabajopractico/sistema/service/WebSecurityConfiguration.java
@@ -1,48 +1,48 @@
-package com.trabajopractico.sistema.service;
+// package com.trabajopractico.sistema.service;
 
-import com.trabajopractico.sistema.filter.JWTFilter;
+// import com.trabajopractico.sistema.filter.JWTFilter;
 
-//import com.trabajopractico.sistema.filter.JWTFilter;
+// //import com.trabajopractico.sistema.filter.JWTFilter;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.authentication.AuthenticationManager;
+// import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+// import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+// import org.springframework.security.config.http.SessionCreationPolicy;
+// import org.springframework.security.core.userdetails.UserDetailsService;
+// import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+// import org.springframework.security.crypto.password.PasswordEncoder;
+// import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+// @Configuration
+// @EnableWebSecurity
+// @EnableGlobalMethodSecurity(prePostEnabled = true)
+// public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	private UserDetailsService userDetailsService;
-	@Autowired
-	private JWTFilter jwtFilter;
+// 	@Autowired
+// 	private UserDetailsService userDetailsService;
+// 	@Autowired
+// 	private JWTFilter jwtFilter;
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService);
-	}
+// 	@Override
+// 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+// 		auth.userDetailsService(userDetailsService);
+// 	}
 
-	@Bean
-	public PasswordEncoder passwordEncoder(){
-		return NoOpPasswordEncoder.getInstance();
-	}
+// 	@Bean
+// 	public PasswordEncoder passwordEncoder(){
+// 		return NoOpPasswordEncoder.getInstance();
+// 	}
 
-	@Bean
-	protected AuthenticationManager authenticationManager() throws Exception {
-		return super.authenticationManager();
-	}
+// 	@Bean
+// 	protected AuthenticationManager authenticationManager() throws Exception {
+// 		return super.authenticationManager();
+// 	}
 
 // 	@Override
 // 	protected void configure(HttpSecurity http) throws Exception {
