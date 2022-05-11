c993fe91257ae4dc04085aa7cbf0110c50b52c11
Facundo Lopez <56969887+f4cuL@users.noreply.github.com>
2022-05-11 18:43:45 -0300

    hotfix

--- src/main/java/com/trabajopractico/sistema/utilities/JWTUtil.java
+++ src/main/java/com/trabajopractico/sistema/utilities/JWTUtil.java
@@ -1,78 +1,78 @@
-// package com.trabajopractico.sistema.utilities;
+package com.trabajopractico.sistema.utilities;
 
-// import java.io.Serializable;
-// import java.util.HashMap;
+import java.io.Serializable;
+import java.util.HashMap;
 
-// import org.springframework.beans.factory.annotation.Value;
-// import org.springframework.security.core.userdetails.UserDetails;
-// import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
-// import io.jsonwebtoken.Claims;
-// import io.jsonwebtoken.Jwts;
-// import io.jsonwebtoken.SignatureAlgorithm;
-// import java.util.Date;
-// import java.util.Map;
-// import java.util.function.Function;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
 
-// @Component
-// public class JWTUtil implements Serializable{
-//     private static final long serialVersionUID = 234234523523L;
+@Component
+public class JWTUtil implements Serializable{
+    private static final long serialVersionUID = 234234523523L;
 
-//     public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
+    public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
 
-//     @Value("salypimienta")
-//     private String secretKey;
+    @Value("salypimienta")
+    private String secretKey;
 
-//     //retrieve username from jwt token
-//     public String getUsernameFromToken(String token) {
-//         return getClaimFromToken(token, Claims::getSubject);
-//     }
+    //retrieve username from jwt token
+    public String getUsernameFromToken(String token) {
+        return getClaimFromToken(token, Claims::getSubject);
+    }
 
-//     //retrieve expiration date from jwt token
-//     public Date getExpirationDateFromToken(String token) {
-//         return getClaimFromToken(token, Claims::getExpiration);
-//     }
-
-
-//     public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
-//         final Claims claims = getAllClaimsFromToken(token);
-//         return claimsResolver.apply(claims);
-//     }
+    //retrieve expiration date from jwt token
+    public Date getExpirationDateFromToken(String token) {
+        return getClaimFromToken(token, Claims::getExpiration);
+    }
 
 
-//     //for retrieving any information from token we will need the secret key
-//     private Claims getAllClaimsFromToken(String token) {
-//         return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
-//     }
+    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(claims);
+    }
 
 
-//     //check if the token has expired
-//     private Boolean isTokenExpired(String token) {
-//         final Date expiration = getExpirationDateFromToken(token);
-//         return expiration.before(new Date());
-//     }
+    //for retrieving any information from token we will need the secret key
+    private Claims getAllClaimsFromToken(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+    }
 
 
-//     //generate token for user
-//     public String generateToken(UserDetails userDetails) {
-//         Map<String, Object> claims = new HashMap<>();
-//         return doGenerateToken(claims, userDetails.getUsername());
-//     }
+    //check if the token has expired
+    private Boolean isTokenExpired(String token) {
+        final Date expiration = getExpirationDateFromToken(token);
+        return expiration.before(new Date());
+    }
 
 
-//     //while creating the token -
-//     //1. Define  claims of the token, like Issuer, Expiration, Subject, and the ID
-//     //2. Sign the JWT using the HS512 algorithm and secret key.
-//     private String doGenerateToken(Map<String, Object> claims, String subject) {
-//         return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
-//                 .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
-//                 .signWith(SignatureAlgorithm.HS512, secretKey).compact();
-//     }
+    //generate token for user
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        return doGenerateToken(claims, userDetails.getUsername());
+    }
 
 
-//     //validate token
-//     public Boolean validateToken(String token, UserDetails userDetails) {
-//         final String username = getUsernameFromToken(token);
-//         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
-//     }
-// }
+    //while creating the token -
+    //1. Define  claims of the token, like Issuer, Expiration, Subject, and the ID
+    //2. Sign the JWT using the HS512 algorithm and secret key.
+    private String doGenerateToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
+                .signWith(SignatureAlgorithm.HS512, secretKey).compact();
+    }
+
+
+    //validate token
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = getUsernameFromToken(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+}
