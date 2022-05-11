566e56c4941df8788b9e15543f117558cfcb3c05
Facundo Lopez <56969887+f4cuL@users.noreply.github.com>
2022-05-11 16:00:23 -0300

    test

--- src/main/java/com/trabajopractico/sistema/service/WebSecurityConfiguration.java
+++ src/main/java/com/trabajopractico/sistema/service/WebSecurityConfiguration.java
@@ -46,7 +46,9 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf()
+		http.cors()
+			.and()
+			.csrf()
 			.disable()
 			.authorizeRequests()
 			.antMatchers("/authenticate")
