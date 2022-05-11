d0130ac8bbb14c1e9401054fca10f23a0017fd7d
Facundo Lopez <56969887+f4cuL@users.noreply.github.com>
2022-05-11 18:33:27 -0300

    hotfix

--- src/main/java/com/trabajopractico/sistema/service/CustomUserDetailsService.java
+++ src/main/java/com/trabajopractico/sistema/service/CustomUserDetailsService.java
@@ -1,28 +1,28 @@
-package com.trabajopractico.sistema.service;
+// package com.trabajopractico.sistema.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.security.core.userdetails.UserDetails;
+// import org.springframework.security.core.userdetails.UserDetailsService;
+// import org.springframework.security.core.userdetails.UsernameNotFoundException;
+// import org.springframework.stereotype.Service;
 
-import com.trabajopractico.sistema.entities.Usuario;
-import com.trabajopractico.sistema.repositories.UsuarioRepository;
+// import com.trabajopractico.sistema.entities.Usuario;
+// import com.trabajopractico.sistema.repositories.UsuarioRepository;
 
-@Service
-public class CustomUserDetailsService implements UserDetailsService {
+// @Service
+// public class CustomUserDetailsService implements UserDetailsService {
 
-	@Autowired
-	private UsuarioRepository usuarioRepository;
+// 	@Autowired
+// 	private UsuarioRepository usuarioRepository;
 	
-	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		Usuario usuario = usuarioRepository.findByNombreUsuario(username);
-		if(usuario==null) {
-			throw new UsernameNotFoundException("usuario no encontrado");
-		}
+// 	@Override
+// 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+// 		Usuario usuario = usuarioRepository.findByNombreUsuario(username);
+// 		if(usuario==null) {
+// 			throw new UsernameNotFoundException("usuario no encontrado");
+// 		}
 		
-		return new CustomUserDetails(usuario);
-	}
+// 		return new CustomUserDetails(usuario);
+// 	}
 
-}
+// }
